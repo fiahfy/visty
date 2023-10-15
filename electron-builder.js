bad0f099bc8b23984844223fb65b0509665db866
fiahfy <d.fiahfy@gmail.com>
2023-10-15 16:44:56 +0900

    update

--- electron-builder.js
+++ electron-builder.js
@@ -1,5 +1,16 @@
+const audioExtensions = require('audio-extensions')
+const videoExtensions = require('video-extensions')
+
 module.exports = {
   appId: 'net.fiahfy.visty',
   asar: true,
   files: ['dist', 'dist-electron'],
+  fileAssociations: [
+    {
+      ext: audioExtensions,
+    },
+    {
+      ext: videoExtensions,
+    },
+  ],
 }
--- electron/main.ts
+++ electron/main.ts
@@ -25,78 +25,78 @@ process.env.VITE_PUBLIC = app.isPackaged
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
-app.whenReady().then(async () => {
-  const trafficLightManager = createTrafficLightManager()
-  const fullscreenManager = createFullscreenManager()
+const trafficLightManager = createTrafficLightManager()
+const fullscreenManager = createFullscreenManager()
 
-  const baseCreateWindow = (state: State) => {
-    const browserWindow = new BrowserWindow({
-      ...state,
-      minHeight: 350,
-      minWidth: 550,
-      titleBarStyle: process.platform === 'darwin' ? 'hidden' : 'default',
-      webPreferences: {
-        preload: join(__dirname, 'preload.js'),
-        webSecurity: !VITE_DEV_SERVER_URL,
-      },
+const baseCreateWindow = (state: State) => {
+  const browserWindow = new BrowserWindow({
+    ...state,
+    minHeight: 350,
+    minWidth: 550,
+    titleBarStyle: process.platform === 'darwin' ? 'hidden' : 'default',
+    webPreferences: {
+      preload: join(__dirname, 'preload.js'),
+      webSecurity: !VITE_DEV_SERVER_URL,
+    },
+  })
+
+  if (VITE_DEV_SERVER_URL) {
+    browserWindow.loadURL(VITE_DEV_SERVER_URL)
+    browserWindow.on('ready-to-show', () => {
+      browserWindow.webContents.openDevTools()
     })
-
-    if (VITE_DEV_SERVER_URL) {
-      browserWindow.loadURL(VITE_DEV_SERVER_URL)
-      browserWindow.on('ready-to-show', () => {
-        browserWindow.webContents.openDevTools()
-      })
-    } else {
-      browserWindow.loadFile(join(process.env.DIST, 'index.html'))
-    }
-
-    trafficLightManager.handle(browserWindow)
-    fullscreenManager.handle(browserWindow)
-
-    return browserWindow
+  } else {
+    browserWindow.loadFile(join(process.env.DIST, 'index.html'))
   }
 
-  const windowManager = createWindowManager(baseCreateWindow)
+  trafficLightManager.handle(browserWindow)
+  fullscreenManager.handle(browserWindow)
 
-  const createWindow = (filePath: string) => {
-    const file = {
-      name: basename(filePath),
-      path: filePath,
-      url: pathToFileURL(filePath).href,
-    }
-    windowManager.create({ file })
+  return browserWindow
+}
+
+const windowManager = createWindowManager(baseCreateWindow)
+
+const createWindow = (filePath: string) => {
+  // TODO: check file
+  const file = {
+    name: basename(filePath),
+    path: filePath,
+    url: pathToFileURL(filePath).href,
   }
+  windowManager.create({ file })
+}
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    // noop
+  }
+})
+
+app.on('before-quit', async () => {
+  await windowManager.save()
+})
+
+app.on('open-file', async (_event, path) => {
+  await app.whenReady()
+  createWindow(path)
+})
+
+app.whenReady().then(async () => {
   registerApplicationMenu(createWindow)
   registerContextMenu()
   registerHandlers()
 
   await windowManager.restore()
-
-  const path = process.argv[1]
-  if (path && path !== '.') {
-    // TODO: check file
-    createWindow(path)
-  }
-
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
-  // explicitly with Cmd + Q.
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      // noop
-    }
-  })
-
-  app.on('before-quit', async () => {
-    await windowManager.save()
-  })
 })
