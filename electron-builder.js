8a9110084b0e5c64e213c92234fdc2fdefeeb150
fiahfy <d.fiahfy@gmail.com>
2023-11-27 23:53:58 +0900

    Update build config

--- electron-builder.js
+++ electron-builder.js
@@ -4,6 +4,9 @@ const videoExtensions = require('video-extensions')
 module.exports = {
   appId: 'net.fiahfy.visty',
   asar: true,
+  directories: {
+    output: 'dist-package',
+  },
   files: ['dist', 'dist-electron'],
   // @see https://github.com/electron-userland/electron-builder/issues/3204
   fileAssociations: [
