3bf684084e557bd014622443bba0aa63e5a1f8f9
fiahfy <d.fiahfy@gmail.com>
2024-06-16 00:57:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      // Ployfill the Electron and Node.js API for Renderer process.
+      // Polyfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the \`nodeIntegration\` needs to be enabled in the Main process.
       // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
       renderer:
