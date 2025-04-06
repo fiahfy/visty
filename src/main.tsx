afc3f5e2e10608082c169a910aec9546b85bcef0
fiahfy <d.fiahfy@gmail.com>
2025-04-06 13:29:42 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,7 @@ import ThemeProvider from '~/providers/ThemeProvider'
 import TrafficLightProvider from '~/providers/TrafficLightProvider'
 import VideoProvider from '~/providers/VideoProvider'
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StoreProvider>
       <ThemeProvider>
